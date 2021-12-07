484bfacfb8810d973f27eade49df941db134c074
Bekarysalashybayev <bekarysalashybaev@gmail.com>
2021-12-07 19:12:44 +0600

    first commit

---
+++ components/header/HeaderBlock.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="HeaderBlock">
+  <div class="header">
+    <div class="burger" @click="toggleSideBar">
+      <button><img src="../../assets/img/Burger.svg" alt=""></button>
+    </div>
+    <div class="logo">
+      <img src="../../assets/img/Logo-MOBILE.svg" alt="">
+    </div>
+    <div class="whatsapp">
+      <img src="../../assets/img/Whatsapp.svg" alt="">
+      <a href="">+7(777) 777 85 75</a>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HeaderBlock",
+  methods:{
+    toggleSideBar(){
+      this.$emit('toggleSideBar')
+    },
+  },
+}
+</script>
+
+<style scoped>
+@import "assets/css/header/headerblock.css";
+</style>
