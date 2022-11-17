5ca2e87d48e9a7ee8d668921747ef98a0d6b7ec6
bekarysalashybayev <bekarysalashybayev@gmail.com>
2022-11-17 19:28:19 +0600

    first commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'TESTADMIN',
+      name: 'TESTENTADMIN',
       exec_mode: 'cluster',
       instances: 'max',
       script: './node_modules/nuxt/bin/nuxt.js',
